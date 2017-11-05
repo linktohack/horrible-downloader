d5981f255ebc78ca2a0d1df379916404511ab551
Quang-Linh LE <ql.le@hotmail.com>
2017-11-05 20:00:02 +0100

    Blacklist

--- index.ts
+++ index.ts
@@ -5,6 +5,20 @@ import * as fetch from 'isomorphic-fetch';
 import * as cheerio from 'cheerio';
 
 const DIR = '/home/link/Downloads/Anime';
+const BLACKLISTS = [
+    '100% Pascal-sensei',
+    'Cardfight!! Vanguard G Z',
+    'Boruto - Naruto Next Generations',
+    'Detective Conan',
+    'Dragon Ball Super',
+    'IDOLiSH7',
+    'Juuni Taisen',
+    'THE iDOLM@STER CINDERELLA GIRLS Theater (TV)',
+    'THE iDOLM@STER CINDERELLA GIRLS Theater (Web)',
+    'The iDOLM@STER Side M',
+    'Yu-Gi-Oh! VRAINS',
+]
+
 console.log(`switched to ${DIR}`);
 process.chdir(DIR);
 
@@ -17,19 +31,23 @@ process.chdir(DIR);
         title: string,
         show: string,
         link: string
-    }[] = $('item').map((idx, el) => {
-        const title = $(el).find('title').text();
-        const matched = title.match(/\[HorribleSubs\] (.*) - [0-9]/);
-        let show = '.'
-        if (matched) {
-            show = matched[1];
-        }
-        const link = $(el).find('link').text();
-        return { title, show, link };
-    }).get() as any;
+    }[] = $('item')
+        .map((idx, el) => {
+            const title = $(el).find('title').text();
+            const matched = title.match(/\[HorribleSubs\] (.*) - [0-9]/);
+            let show = '.'
+            if (matched) {
+                show = matched[1];
+            }
+            const link = $(el).find('link').text();
+            return { title, show, link };
+        })
+        .get() as any;
+
+    const filteredItems = items.filter(it => BLACKLISTS.indexOf(it.show) === -1);
 
     await new Promise((resovle, reject) => {
-        writeFile('link.txt', items.map(it => `${it.link}\n dir=${it.show}`).join('\n'), resovle);
+        writeFile('link.txt', filteredItems.map(it => `${it.link}\n dir=${it.show}`).join('\n'), resovle);
     });
 
     await new Promise((resolve, reject) => {
