dec8962fef9f9ec326d154a52f24f887781d9900
Quang-Linh LE <ql.le@hotmail.com>
2017-11-19 21:47:51 +0100

    Ignore already downloaded file

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs';
+import { writeFile, appendFile, readFile, createWriteStream } from 'fs';
 import { spawn } from 'child_process';
 
 import * as fetch from 'isomorphic-fetch';
@@ -9,6 +9,8 @@ const BLACKLISTS = [
     // Winter 2017
     '100% Pascal-sensei',
     '3-gatsu no Lion',
+    'Animegataris',
+    'Ballroom e Youkoso',
     'Boruto - Naruto Next Generations',
     'Cardfight!! Vanguard G Z',
     'Detective Conan',
@@ -16,7 +18,9 @@ const BLACKLISTS = [
     'Dynamic Chord',
     'IDOLiSH7',
     'Juuni Taisen',
+    'Gintama',
     'One Piece',
+    'Onyankopon',
     'Ousama Game',
     'Puzzle and Dragons Cross',
     'THE iDOLM@STER CINDERELLA GIRLS Theater (TV)',
@@ -27,10 +31,11 @@ const BLACKLISTS = [
     'Wake Up, Girls! Shin Shou',
     'Youkai Apartment no Yuuga na Nichijou',
     'Yu-Gi-Oh! VRAINS',
-]
+    'Folktales from Japan S2',
+];
 
-console.log(`switched to ${DIR}`);
 process.chdir(DIR);
+console.log(`Switched to ${process.cwd()}`);
 
 (async () => {
     const res = await fetch('http://horriblesubs.info/rss.php?res=1080');
@@ -54,20 +59,39 @@ process.chdir(DIR);
         })
         .get() as any;
 
-    const filteredItems = items.filter(it => BLACKLISTS.indexOf(it.show) === -1);
+    const completed = await new Promise<string[]>((resolve, reject) => {
+        readFile('link-completed.txt', (err, data) => {
+            if (err) {
+                return resolve([]);
+            }
+            return resolve(data.toString().split('\n'));
+        })
+    });
 
-    await new Promise((resovle, reject) => {
-        writeFile('link.txt', filteredItems.map(it => `${it.link}\n dir=${it.show}`).join('\n'), resovle);
+    const filteredItems = items
+        .filter(it => BLACKLISTS.indexOf(it.show) === -1)
+        .filter(it => completed.indexOf(it.link) === -1);
+
+    console.log(`File to download: ${filteredItems.map(it => `\n  ${it.title}`).join('')}`);
+
+    await new Promise((resolve, reject) => {
+        writeFile('link.txt', filteredItems.map(it => `${it.link}\n dir=${it.show}`).join('\n'), resolve);
     });
 
     await new Promise((resolve, reject) => {
-        const process = spawn(`aria2c`, [`--seed-time`, `0`, `-j`, `${items.length}`, `-i`, `link.txt`]);
-        process.on('close', resolve);
-        process.stdout.on('data', data => console.log(data.toString()));
-        process.stderr.on('data', data => console.log(data.toString()));
+        const process = spawn('aria2c', ['-c', '--seed-time', '0', '-j', `${items.length}`, '-i', 'link.txt']);
+        process.on('close', (code) => {
+            (code === 0 || code === 13) ? resolve(code) : reject(code); // 13: file already existed
+        });
+        process.stdout.pipe(createWriteStream('link-stdout.txt', {flags: 'a'}));
+        process.stderr.pipe(createWriteStream('link-stderr.txt', {flags: 'a'}));
     });
 
-    console.log('Exited happily!')
+    await new Promise((resolve, reject) => {
+        appendFile('link-completed.txt', filteredItems.map(it => `${it.link}\n`).join(''), resolve);
+    });
+
+    console.log('Exited happily!');
 })()
     .catch(error => {
         console.log('Caught error:', error);
